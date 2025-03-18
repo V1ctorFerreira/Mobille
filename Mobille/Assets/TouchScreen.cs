818749988a2737c35bc918304640394e635cb479
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-18 11:36:14 -0300

    Modificando codigo

--- Mobille/Assets/TouchScreen.cs
+++ Mobille/Assets/TouchScreen.cs
@@ -5,14 +5,31 @@ using UnityEngine.Events;
 public class TouchScreen : MonoBehaviour
 {
   public UnityEvent OnTouch;
-   
+    Vector2 inicialPos;
 
   
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //if (Input.GetMouseButtonDown(0))
+      //  {
+          //  OnTouch.Invoke();
+      //  }
+        if (Input.GetButtonDown("Fire1"))
         {
-            OnTouch.Invoke();
-        } 
+            inicialPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Vector2 finalPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(finalPos.x < inicialPos.x)
+            {
+
+                print("Esquerda");
+            }
+            if(finalPos.x > inicialPos.x)
+            {
+                print("Direita");
+            }
+        }
     }
 }
